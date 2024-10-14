c8357e5881a490a65f619140caf5091f386970b0
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-14 16:50:21 -0500

    minor change

--- test_bisection_method.m
+++ test_bisection_method.m
@@ -1,9 +1,6 @@
 clc, clearvars;
 
-a = 30;
-b = 40;
-
-disp(bisection_method(a, b, @f));
+disp(bisection_method(30, 40, @f));
 
 function R = f(x)
     R = (5/3)*cosd(40) - (5/2)*cosd(x) + (11/6) - cosd(40 - x);
