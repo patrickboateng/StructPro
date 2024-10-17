2c12d79752c19a141a4821b2c502a29ec7584ccc
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-17 18:08:54 -0500

    refactored code

--- bisection_method.m
+++ bisection_method.m
@@ -1,46 +1,37 @@
-
-function X = bisection_method(a, b, f, MAX_ITER)
+function avg = bisection_method(a, b, f, opts)
 arguments
     a (1, 1) {mustBeNumeric}
     b (1, 1) {mustBeNumeric}
     f {mustBeA(f, "function_handle")}
-    MAX_ITER (1, 1) {mustBeInteger} = 10000
+    opts.TOL (1, 1) {mustBeInteger} = 1e-6
+    opts.MAX_ITER (1, 1) {mustBeInteger} = 10000
 end
-
-avg = (a + b) / 2;
 
 f_a = f(a);
 f_b = f(b);
-f_avg = f(avg);
-
-TOL = 1e-6;
 
 idx = 1;
 
-while f_a * f_b < 0
+while idx < opts.MAX_ITER
+
+    avg = (a + b) / 2;
+    f_avg = f(avg);
+    
+    if ~ (f_a * f_b < 0)
+        break
+    end
     
     if f_avg < 0 && f_a < 0 || f_avg > 0 && f_a > 0
         a = avg;
-    end
-
-    if f_avg < 0 && f_b < 0 || f_avg > 0 && f_b > 0
+    else
         b = avg;
     end
 
-    avg = (a + b) / 2;
-
-    f_avg = f(avg);
-
-    if abs(b - a) < TOL
+    if abs(b - a) < opts.TOL
         break
-    end
-        
+    end     
+
     idx = idx + 1;
-
-    if idx > MAX_ITER
-        break
-    end
-    
 end
-X = avg;
+avg = (a + b) / 2;
 end
