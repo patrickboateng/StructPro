2f544b1f3da6c5d141090a9a66285af2ae10217c
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-17 18:21:58 -0500

    minor code refactoring

--- newtons_method.m
+++ newtons_method.m
@@ -1,23 +1,23 @@
 
 
-function x = newtons_method(x, f, d_f, TOL, MAX_ITER)
+function new_x = newtons_method(x, f, d_f, opts)
 arguments
     x {mustBeNumeric}
     f {mustBeA(f, "function_handle")}
     d_f {mustBeA(d_f, "function_handle")}
-    TOL {mustBeFloat} = 1e-6
-    MAX_ITER {mustBeInteger} = 1000
+    opts.TOL {mustBeFloat} = 1e-6
+    opts.MAX_ITER {mustBeInteger} = 1000
 end
 
 old_x = x;
 
 idx = 1;
 
-while idx < MAX_ITER
+while idx < opts.MAX_ITER
 
 new_x = old_x - f(old_x) / d_f(old_x);
 
-if abs(old_x - new_x) < TOL
+if abs(old_x - new_x) < opts.TOL
     break
 end
 
@@ -25,7 +25,4 @@ old_x = new_x;
 
 idx = idx + 1;
 end
-    
-x = new_x;
-
 end
