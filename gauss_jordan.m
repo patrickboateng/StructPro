27f278b2855139c5c1334a494979ec866ff5e5d0
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-17 21:18:49 -0500

    code refactoring

--- gauss_jordan.m
+++ gauss_jordan.m
@@ -1,9 +1,9 @@
 
-function x = gauss_jordan(A, b, TOL)
+function unknown_x_vars = gauss_jordan(A, b, opts)
 arguments
     A (:, :) double {mustBeMatrix, mustBeSquareMatrix}
     b  double {mustBeColumn}
-    TOL {mustBeFloat} = 1e-6
+    opts.TOL {mustBeFloat} = 1e-6
 end
 
 % r will serve as the total number of iterations.
@@ -14,44 +14,40 @@ augMat = [A b];
 
 % Forward Elimination
 for i=1:r
-max_ = i;
-for m =i+1:r
-    if abs(augMat(max_, max_)) > TOL
-        break;
+    max_ = i;
+    for m =i+1:r
+        if abs(augMat(max_, max_)) > opts.TOL
+            break;
+        end
+        
+        if abs(augMat(m, i)) > abs(augMat(i, i))
+            max_ = m;
+        end
     end
-    
-if abs(augMat(m, i)) > abs(augMat(i, i))
-    max_ = m;
-end
-end
 
 if max_ ~= i
-tmp = augMat(i, :);
-augMat(i, :) = augMat(max_, :);
-augMat(max_, :) = tmp;
+    tmp = augMat(i, :);
+    augMat(i, :) = augMat(max_, :);
+    augMat(max_, :) = tmp;
 end
 
-if abs(augMat(i, i)) < TOL, error("Singular Matrix")
+if abs(augMat(i, i)) < opts.TOL, error("Singular Matrix")
 end 
 
 for j=i+1:r
-augMat(i, :) = augMat(i, :) ./ augMat(i, i);
-tr_vec = augMat(i, :) .* augMat(j, i);
-augMat(j, :) = augMat(j, :) - tr_vec;
+    augMat(i, :) = augMat(i, :) ./ augMat(i, i);
+    augMat(j, :) = augMat(j, :) - augMat(i, :) .* augMat(j, i);
 end
-
 augMat(i, :) = augMat(i, :) ./ augMat(i, i);
-
 end
 
 % Backward Elimination
 for i=r:-1:1
    for j=i-1:-1:1
-   tr_vec = augMat(i, :) .* augMat(j, i);
-   augMat(j, :) = augMat(j, :) - tr_vec;
+    augMat(j, :) = augMat(j, :) - augMat(i, :) .* augMat(j, i);
    end
 end
-x = augMat(:, end);
+unknown_x_vars = augMat(:, end);
 end
 
 function mustBeSquareMatrix(A)
