399b1f7e083bb03802cdb3256c45425fae22a084
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-17 21:31:09 -0500

    minor change

--- newtons_method.m
+++ newtons_method.m
@@ -1,18 +1,17 @@
 
 
-function new_x = newtons_method(x, f, d_f, opts)
+function soln = newtons_method(x0, f, d_f, opts)
 arguments
-    x {mustBeNumeric}
+    x0 {mustBeNumeric}
     f {mustBeA(f, "function_handle")}
     d_f {mustBeA(d_f, "function_handle")}
     opts.TOL {mustBeFloat} = 1e-6
     opts.MAX_ITER {mustBeInteger} = 1000
 end
 
-old_x = x;
+old_x = x0;
 
 idx = 1;
-
 while idx < opts.MAX_ITER
 
 new_x = old_x - f(old_x) / d_f(old_x);
@@ -22,7 +21,8 @@ if abs(old_x - new_x) < opts.TOL
 end
 
 old_x = new_x;
-
 idx = idx + 1;
+
 end
+soln = new_x;
 end
