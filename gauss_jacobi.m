b2abc844cddfcfb1cac9c4716b847978103a1736
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-17 20:49:49 -0500

    added logic to check if solution is converging or diverging

--- gauss_jacobi.m
+++ gauss_jacobi.m
@@ -23,10 +23,18 @@ while idx < opts.MAX_ITER
         end
         unknown_vars(i) = (b(i) - x) / A(i, i);
     end
-    % Check the error tolerance
-    if norm(unknown_vars - x_old) < opts.TOL
+    new_error = norm(unknown_vars - x_old);
+    
+    if new_error < opts.TOL
         break
     end
+    
+    if idx > 1
+        if ~(new_error - old_error < 0)
+            warning("Solution is not converging.")
+        end
+    end
+    old_error = new_error;
     idx = idx + 1;
 end 
 end
--- gauss_seidel.m
+++ gauss_seidel.m
@@ -1,67 +1,41 @@
-
-function X = gauss_seidel(A, b, TOL, MAX_ITER)
+function unknown_vars = gauss_seidel(A, b, opts)
 arguments
     A (:, :) double {mustBeMatrix}
     b  double {mustBeColumn}
-    TOL {mustBeFloat} = 1e-6
-    MAX_ITER {mustBeInteger} = 1000
+    opts.TOL {mustBeFloat} = 1e-6
+    opts.MAX_ITER {mustBeInteger} = 1000
 end
 
 [r, ~] = size(A);
 
 unknown_vars = zeros(r, 1);
 
-% global_errors = zeros(1, r);
+idx = 0;
 
-% iter = 1;
-iter = 0;
-
-while iter < MAX_ITER
-x_old = unknown_vars;
-for i=1:r
-x = 0;
-for j=1:r
-    if i==j
-        continue
+while idx < opts.MAX_ITER
+    x_old = unknown_vars;
+    for i=1:r
+        x = 0;
+        for j=1:r
+            if i ~= j
+                x = x + A(i, j) * unknown_vars(j);
+            end
+        end
+        unknown_vars(i) = (b(i) - x) / A(i, i);
     end
-    x = x + A(i, j) * unknown_vars(j);
-end
-unknown_vars(i) = (b(i) - x) / A(i, i);
-end
+
+    new_error = norm(unknown_vars - x_old);
     
-        % Calculate error for each variable
-        % local_errors = zeros(1, r);
-        % for k=1:r
-        %     err = ((unknown_vars(k) - x_old(k)) / unknown_vars(k)) * 100;
-        %     local_errors(k) = abs(err);
-        % end
-        % 
-        % global_errors(iter, :) = local_errors;
-        % 
-        % iter = iter + 1;
+    if new_error < opts.TOL
+        break
+    end
     
-        % Check the error tolerance
-        % if max(local_errors) < tol
-        %     break
-        % end
-if norm(unknown_vars - x_old) < TOL
-    break
+    if idx > 1
+        if ~(new_error - old_error < 0)
+            warning("Solution is not converging.")
+        end
+    end
+    old_error = new_error;
+    idx = idx + 1;
 end
-
-iter = iter + 1;
 end
-X = unknown_vars;
-end
-% disp(global_errors);
-% disp(unknown_vars);
-
-% number_of_iterations = 1:length(global_errors);
-% 
-% for it=1:r
-%     plot(number_of_iterations, global_errors(:, it))
-%     hold on
-% end
-% legend
-% title("A graph of Errors against No. of iterations")
-
-% help plot
