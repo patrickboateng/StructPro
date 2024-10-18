c44c93cbba45ecf6889fcc13777bc4ffc4741459
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-17 21:28:08 -0500

    code refactoring

--- mat_inv.m
+++ mat_inv.m
@@ -2,7 +2,7 @@
 
 % A = [3, -0.1, -0.2; 0.1, 7, -0.3; 0.3, -0.2, 10];
 
-function X = mat_inv(A)
+function inv = mat_inv(A)
 arguments
     A (:, :) {mustBeMatrix}
 end
@@ -19,63 +19,56 @@ TOL = 1e-6;
 % Forward Elimination
 for i=1:r
 % find pivot and swap
-max_ = i;
-for m =i+1:r
-    if abs(U(max_, max_)) > TOL
-        break;
+    max_ = i;
+    for m =i+1:r
+        if abs(U(max_, max_)) > TOL
+            break;
+        end
+    
+        if abs(U(m, i)) > abs(U(i, i))
+            max_ = m;
+        end
     end
 
-    if abs(U(m, i)) > abs(U(i, i))
-        max_ = m;
+    if max_ ~= i
+        tmp = U(i, :);
+        U(i, :) = U(max_, :);
+        U(max_, :) = tmp;
     end
-end
 
-if max_ ~= i
-    tmp = U(i, :);
-    U(i, :) = U(max_, :);
-    U(max_, :) = tmp;
-end
-
-if abs(U(i, i)) < TOL, error("Singular Matrix")
-end 
-
-for j=i+1:r
-    factor = (U(j, i) / U(i, i));
-    tr_vec = U(i, :) .* factor;
-    U(j, :) = U(j, :) - tr_vec;
-    L(j, i) = factor;
-end
+    if abs(U(i, i)) < TOL, error("Singular Matrix")
+    end 
+    
+    for j=i+1:r
+        factor = (U(j, i) / U(i, i));
+        U(j, :) = U(j, :) - U(i, :) .* factor;
+        L(j, i) = factor;
+    end
 end
 
 I = eye(r);
-
 invMat = zeros(r);
 
 for i=1:r
+    b = I(:, i);
+    unknown_z_vars = zeros(r, 1);
+    
+    % Forward elimination
+    for j=1:r
+        x = L(j, :) * unknown_z_vars;
+        x = (b(j) - x) / L(j, j);
+        unknown_z_vars(j, 1) = x;
+    end
+    
+    unknown_x_vars = zeros(r, 1);
 
-b = I(:, i);
-unknown_z_vars = zeros(r, 1);
-
-% Forward elimination
-for j=1:r
-    x = L(j, :) * unknown_z_vars;
-    x = (b(j) - x) / L(j, j);
-    unknown_z_vars(j, 1) = x;
+    % Backward elimination
+    for k=r:-1:1
+        x = U(k, :) * unknown_x_vars;
+        x = (unknown_z_vars(k) - x) / U(k, k);
+        unknown_x_vars(k, 1) = x;
+    end   
+    invMat(:, i) = unknown_x_vars;
 end
-
-unknown_x_vars = zeros(r, 1);
-
-% Backward elimination
-for k=r:-1:1
-    x = U(k, :) * unknown_x_vars;
-    x = (unknown_z_vars(k) - x) / U(k, k);
-    unknown_x_vars(k, 1) = x;
-end
-
-invMat(:, i) = unknown_x_vars;
-
-end
-
-X = invMat;
-
+    inv = invMat;
 end
