fed41b8fe620ff1f068da725c4eff053260392d7
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-17 20:23:42 -0500

    code refactoring

--- gauss_jacobi.m
+++ gauss_jacobi.m
@@ -1,35 +1,32 @@
-
-function X = gauss_jacobi(A, b, TOL, MAX_ITER)
+function unknown_vars = gauss_jacobi(A, b, opts)
 arguments
     A (:, :) double {mustBeMatrix}
     b  double {mustBeColumn}
-    TOL {mustBeFloat} = 1e-6
-    MAX_ITER {mustBeInteger} = 1000
+    opts.TOL {mustBeFloat} = 1e-6
+    opts.MAX_ITER {mustBeInteger} = 1000
 end
 
 [r, ~] = size(A);
 
 unknown_vars = zeros(r, 1);
 
-iter = 0;
+idx = 1;
 
-while iter < MAX_ITER
+while idx < opts.MAX_ITER
     x_old = unknown_vars;
     for i=1:r
         x = 0;
         for j=1:r
-            if i==j
-                continue
+            if i ~= j
+                x = x + A(i, j) * x_old(j);
             end
-            x = x + A(i, j) * x_old(j);  
         end
         unknown_vars(i) = (b(i) - x) / A(i, i);
     end
     % Check the error tolerance
-    if norm(unknown_vars - x_old) < TOL
+    if norm(unknown_vars - x_old) < opts.TOL
         break
     end
-    iter = iter + 1;
-end
-X = unknown_vars;   
+    idx = idx + 1;
+end 
 end
