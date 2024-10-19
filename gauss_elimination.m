e3484ca2ba712fbaaf3277eddcd95ff8b51d1fd9
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-19 12:07:43 -0500

    moved the swap algorithm to a separate private function

--- gauss_elimination.m
+++ gauss_elimination.m
@@ -8,35 +8,17 @@ end
 % r will serve as the total number of iterations.
 [r, ~] = size(A);
 
-% Augmented matrix
-augMat = [A b];
+% Augmented matrix assigned to A for code optimization.
+A = [A b]; 
 
 % Forward Elimination
 for i=1:r
     % find pivot and swap
-    max_ = i;
-    for m =i+1:r
-        if abs(augMat(max_, max_)) > opts.TOL
-            break;
-        end
-
-        if abs(augMat(m, i)) > abs(augMat(i, i))
-            max_ = m;
-        end
-    end
-    
-    if max_ ~= i
-        tmp = augMat(i, :);
-        augMat(i, :) = augMat(max_, :);
-        augMat(max_, :) = tmp;
-    end
-
-    if abs(augMat(i, i)) < opts.TOL, error("Singular Matrix")
-    end 
+    A = swap(A, pv_idx=1, total_rows=r, TOL=opts.TOL);
 
     for j=i+1:r
-        tr_vec = augMat(i, :) .* (augMat(j, i) / augMat(i, i));
-        augMat(j, :) = augMat(j, :) - tr_vec;
+        tr_vec = A(i, :) .* (A(j, i) / A(i, i));
+        A(j, :) = A(j, :) - tr_vec;
     end
 end
 
@@ -44,8 +26,8 @@ unknown_x_vars = zeros(r, 1);
 
 % Backward Substitution
 for i=r:-1:1
-    x = augMat(i, 1:r) * unknown_x_vars;
-    x = (augMat(i, end) - x)/ augMat(i, i);
+    x = A(i, 1:r) * unknown_x_vars;
+    x = (A(i, end) - x)/ A(i, i);
     unknown_x_vars(i, 1) = x;
 end
 end
