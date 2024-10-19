69339caad6cd8847b5461157da1e139bfec77c12
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-19 13:14:22 -0500

    added new set of tests cases

--- test_linear_numerical_methods.m
+++ test_linear_numerical_methods.m
@@ -3,6 +3,9 @@ clc;clearvars;
 A = [0.3 0.52 1.0; 0.5 1.0 1.9; 0.1 0.3 0.5];
 b = [-0.01; 0.67; -0.44];
 
+% A = [1 -1 2; 0 0 -1; 0 2 -1];
+% b = [8; -11; -3];
+
 A = [4 1 -1; 2 7 1; 1 -3 12];
 b = [3; 19; 31];
 
@@ -10,6 +13,7 @@ b = [3; 19; 31];
 % disp(gauss_elimination(A, b))
 % disp(gauss_jacobi(A, b))
 % disp(gauss_seidel(A, b))
-% disp(lu_decomposition(A, b)) 
+% disp(lu_decomposition(A, b))
 % disp(gauss_jordan(A, b))
-disp(mat_inv(A) * A)
+disp(mat_inv(A))
+disp(A^-1)
