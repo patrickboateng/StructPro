3f7ba968879145f00bcd9104b1d7932f551035da
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-19 11:32:25 -0500

    converted local functions to anonymous functions

--- test_newtons_method.m
+++ test_newtons_method.m
@@ -1,11 +1,6 @@
 clc;clearvars;
 
-function R = f(x)
-R = (5/3)*cosd(40) - (5/2)*cosd(x) + (11/6) - cosd(40 - x);
-end
+f = @(x) (5/3)*cosd(40) - (5/2)*cosd(x) + (11/6) - cosd(40 - x);
+d_f = @(x) (5/2 * sind(x) - sind(40 - x)) * (pi/180);
 
-function R = d_f(x)
-R = (5/2 * sind(x) - sind(40 - x)) * (pi/180);
-end
-
-disp(newtons_method(30, @f, @d_f))
+disp(newtons_method(30, f, d_f))
--- test_secant_method.m
+++ test_secant_method.m
@@ -1,7 +1,5 @@
 clc;clearvars;
 
-function R = f(x)
-R = (5/3)*cosd(40) - (5/2)*cosd(x) + (11/6) - cosd(40 - x);
-end
+f = @(x) (5/3)*cosd(40) - (5/2)*cosd(x) + (11/6) - cosd(40 - x);
 
-disp(secant_method(30, 40, @f));
+disp(secant_method(30, 40, f));
