b6eed4ea710683e77d274d9cfabd619f8681588b
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-19 15:35:07 -0500

    converted local function to anonymous function

--- src/test_bisection_method.m
+++ src/test_bisection_method.m
@@ -1,7 +1,5 @@
 clc, clearvars;
 
-disp(bisection_method(30, 40, @f));
+f = @(x) (5/3)*cosd(40) - (5/2)*cosd(x) + (11/6) - cosd(40 - x);
 
-function R = f(x)
-    R = (5/3)*cosd(40) - (5/2)*cosd(x) + (11/6) - cosd(40 - x);
-end
+disp(bisection_method(30, 40, @f));
