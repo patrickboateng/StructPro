41d2ac49dfd52cee525a04ef23f36c45b6fe57de
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-22 21:53:38 -0500

    returns the permutation matrix for swapping rows

--- src/private/swapRow.m
+++ src/private/swapRow.m
@@ -1,31 +1,43 @@
-function A = swapRow(A, opts)
+function [P, A] = swapRow(A, P, opts)
 arguments
-    A 
+    A
+    P
     opts.pv_idx
     opts.total_rows
     opts.tol
 end
 
-max_ = opts.pv_idx;
+if ~(isempty(P))
+    mustBeMatrix(P);
+end
+
+max_idx = opts.pv_idx;
 
 % Select pivot
 for m =opts.pv_idx+1:opts.total_rows
-    if abs(A(max_, max_)) > opts.tol
-        break;
+    if abs(A(max_idx, max_idx)) > opts.tol
+        break
     end
-
     if abs(A(m, opts.pv_idx)) > abs(A(opts.pv_idx, opts.pv_idx))
-        max_ = m;
+        max_idx = m;
     end
 end
 
 % Swap
-if max_ ~= opts.pv_idx
-    tmp = A(opts.pv_idx, :);
-    A(opts.pv_idx, :) = A(max_, :);
-    A(max_, :) = tmp;
+if max_idx ~= opts.pv_idx
+    A = swap(A, opts.pv_idx, max_idx);
+    if ~isempty(P)
+        P = swap(P, opts.pv_idx, max_idx);
+    end
 end
 
-if abs(A(opts.pv_idx, opts.pv_idx)) < opts.tol, error("Singular Matrix")
-end 
+if abs(A(opts.pv_idx, opts.pv_idx)) < opts.tol
+    error("Singular Matrix")
+end
+end
+
+function A = swap(A, idx1, idx2)
+tmp = A(idx1, :);
+A(idx1, :) = A(idx2, :);
+A(idx2, :) = tmp;
 end
