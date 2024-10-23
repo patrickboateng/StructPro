e33145403d1ddd28450f596e37629e8efb8ec0c2
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-22 22:16:45 -0500

    used new signature of function

--- src/lu_decomposition.m
+++ src/lu_decomposition.m
@@ -4,24 +4,9 @@ arguments
     b  double {mustBeColumn}
     opts.tol {mustBeFloat} = 1e-6
 end
-
-% r will serve as the total number of iterations.
 [r, ~] = size(A);
-
-% Lower triangular matrix
-L = eye(r);
-
-% Forward Elimination
-for i=1:r
-    % find pivot and swap if zero or close to zero.
-    A = swap(A, pv_idx=i, total_rows=r, tol=opts.tol);
-
-    for j=i+1:r
-        factor = (A(j, i) / A(i, i));
-        A(j, :) = A(j, :) - A(i, :) .* factor;
-        L(j, i) = factor;
-    end
-end
+[P, L, U] = lu_factor(A, tol=opts.tol);
+b = P * b;
 
 unknown_z_vars = zeros(r, 1);
 
@@ -35,7 +20,7 @@ unknown_x_vars = zeros(r, 1);
 
 % Backward Substitution
 for i=r:-1:1
-    x = A(i, :) * unknown_x_vars;
-    unknown_x_vars(i, 1) = (unknown_z_vars(i) - x)/ A(i, i);
+    x = U(i, :) * unknown_x_vars;
+    unknown_x_vars(i, 1) = (unknown_z_vars(i) - x)/ U(i, i);
 end
 end
--- src/mat_inv.m
+++ src/mat_inv.m
@@ -6,24 +6,9 @@ end
 % r will serve as the total number of iterations.
 [r, c] = size(A);
 
-% Lower triangular matrix
-L = eye(r);
+[P, L, U] = lu_factor(A, tol=1e-6);
 
-tol = 1e-6;
-
-% Forward Elimination
-for i=1:r
-    % find pivot and swap if zero or close to zero.
-    A = swap(A, pv_idx=i, total_rows=r, TOL=tol);
-
-    for j=i+1:r
-        factor = (A(j, i) / A(i, i));
-        A(j, :) = A(j, :) - A(i, :) .* factor;
-        L(j, i) = factor;
-    end
-end
-
-I = eye(r);
+I = P * eye(r, c);
 invMat = zeros(r, c);
 
 for i=1:r
@@ -40,8 +25,8 @@ for i=1:r
 
     % Backward elimination
     for k=r:-1:1
-        x = A(k, :) * unknown_x_vars;
-        unknown_x_vars(k, 1) = (unknown_z_vars(k) - x) / A(k, k);
+        x = U(k, :) * unknown_x_vars;
+        unknown_x_vars(k, 1) = (unknown_z_vars(k) - x) / U(k, k);
     end
     invMat(:, i) = unknown_x_vars;
 end
