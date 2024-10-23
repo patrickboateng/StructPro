f26a830c609ff6b4a67e5d4c0bf81a6af00ec022
Patrick Boateng <boatengpato.pb@gmail.com>
2024-10-22 21:58:09 -0500

    added the permutation matrix to the returned values

--- src/lu_factor.m
+++ src/lu_factor.m
@@ -1,4 +1,4 @@
-function [L, U] = lu_factor(A, opts)
+function [P, L, U] = lu_factor(A, opts)
 arguments
     A double {mustBeMatrix}
     opts.tol {mustBeFloat} = 1e-6
@@ -9,10 +9,13 @@ end
 % Lower triangular matrix
 L = eye(r);
 
+% Row permutation matrix
+P = eye(r);
+
 % Forward Elimination
 for i=1:r
     % find pivot and swap if zero or close to zero.
-    A = swapRow(A, pv_idx=i, total_rows=r, tol=opts.tol);
+    [P, A] = swapRow(A, P, pv_idx=i, total_rows=r, tol=opts.tol);
 
     for j=i+1:r
         factor = (A(j, i) / A(i, i));
