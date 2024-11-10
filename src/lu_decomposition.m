3ad6d0eed1f04c7b0b872055f2ceb911151ad6de
Patrick Boateng <boatengpato.pb@gmail.com>
2024-11-09 23:24:24 -0600

    minor change

--- src/lu_decomposition.m
+++ src/lu_decomposition.m
@@ -13,7 +13,7 @@ unknown_z_vars = zeros(r, 1);
 % Forward Substitution
 for i=1:r
     x = L(i, :) * unknown_z_vars;
-    unknown_z_vars(i, 1) = (b(i) - x)/ L(i, i);
+    unknown_z_vars(i, 1) = (b(i) - x) / L(i, i);
 end
 
 unknown_x_vars = zeros(r, 1);
@@ -21,6 +21,6 @@ unknown_x_vars = zeros(r, 1);
 % Backward Substitution
 for i=r:-1:1
     x = U(i, :) * unknown_x_vars;
-    unknown_x_vars(i, 1) = (unknown_z_vars(i) - x)/ U(i, i);
+    unknown_x_vars(i, 1) = (unknown_z_vars(i) - x) / U(i, i);
 end
 end
