df470e211f9fd57ab4b533b613ab153cecb6a544
Patrick Boateng <boatengpato.pb@gmail.com>
2024-11-11 21:38:09 -0600

    solver for non-linear systems of equations

---
+++ src/newtons_diff_solver.m
@@ -0,0 +1,27 @@
+function vars_i = newtons_diff_solver(funcs, vars, vars_i)
+arguments
+    funcs (1, :) {mustBeRow}
+    vars (1, :)  {mustBeRow}
+    vars_i (1, :) double {mustBeRow}
+end
+
+[~, c] = size(vars);
+A = size(c);
+b = zeros(c, 1);
+iter = 1;
+
+while iter < 100
+    for i=1:c
+        for j=1:c
+            A(i, j) = subs(diff(funcs(i), vars(j)), vars_i(j));
+        end
+        b(i) = -1 * subs(funcs(i), vars, vars_i);
+    end
+    soln = A \ b;
+    if sum(abs(soln)) < 1e-4
+        break;
+    end
+    vars_i = vars_i + soln';
+    iter = iter + 1;
+end
+end
