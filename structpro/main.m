145486ca65540eb6cc9dfb41c94e86f90028b2d4
Patrick Boateng <boatengpato.pb@gmail.com>
2024-11-18 15:51:59 -0600

    added additional testcase

--- structpro/main.m
+++ structpro/main.m
@@ -1,7 +1,7 @@
 clc;clearvars;
 
 node_1 = Node(1, 0, 0, 0, 0, 1, 1, 1);
-node_2 = Node(2, 5, 0, -5, -10, 0, 0, 0);
+node_2 = Node(2, 5, 0, -5, 10, 0, 0, 0);
 node_3 = Node(3, 10, 0, 0, 0, 1, 1, 1);
 % 
 section = RectangularSection(0.2, 0.4, 200e9);
@@ -20,14 +20,21 @@ beam.add_member(member_1);
 beam.add_member(member_2);
 % 
 
-% p_load = PointLoad(-5, node_2);
-
-% moment = PointMoment(10, node_2);
-
-% beam.add_point_load(p_load);
-
-% beam.add_point_moment(moment);
-
+% node_1 = Node(1, 0, 0, 0, 0, 1, 1, 0);
+% node_2 = Node(2, 2, 0, 0, 0, 0, 1, 0);
+% node_3 = Node(3, 4, 0, -5, 0, 0, 0, 0);
+% 
+% member_1 = Member(1, node_1, node_2, section);
+% member_2 = Member(2, node_2, node_3, section);
+% 
+% beam = Beam(4);
+% 
+% beam.add_node(node_1);
+% beam.add_node(node_2);
+% beam.add_node(node_3);
+% 
+% beam.add_member(member_1);
+% beam.add_member(member_2);
 
 Solver.solve(beam)
 
