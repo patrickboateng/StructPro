f2fbae61f9003297da72456df1fc5f9811dcb366
Patrick Boateng <boatengpato.pb@gmail.com>
2024-11-18 15:47:28 -0600

    added member length and member stiffness matrix

--- structpro/Member.m
+++ structpro/Member.m
@@ -13,5 +13,24 @@ classdef Member
             obj.end_node = node_2;
             obj.section = section;
         end
+
+        function len = member_length(obj)
+            len = obj.end_node.position.x - obj.start_node.position.x;
+        end
+
+        function m_stiffness = member_stiffness_matrix(obj)
+            m_len = obj.member_length();
+            E = obj.section.E;
+            I = obj.section.I;
+
+            k = (E * I) / m_len^3;
+
+            m_stiffness = [12 6*m_len -12 6*m_len; ...
+                6*m_len 4*m_len^2 -6*m_len 2*m_len^2; ...
+                -12 -6*m_len 12 -6*m_len; ...
+                6*m_len 2*m_len^2 -6*m_len 4*m_len^2];
+
+            m_stiffness = m_stiffness .* k;
+        end
     end
 end
