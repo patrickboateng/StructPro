df0ddc26752cc2c6ec9d4c677d47b2fa2364965d
Patrick Boateng <boatengpato.pb@gmail.com>
2024-11-18 23:21:25 -0600

    corrected the algorithm

--- src/newtons_diff_solver.m
+++ src/newtons_diff_solver.m
@@ -1,8 +1,9 @@
-function vars_i = newtons_diff_solver(funcs, vars, vars_i)
+function vars_i = newtons_diff_solver(funcs, vars, vars_i, opts)
 arguments
     funcs (1, :) {mustBeRow}
     vars (1, :)  {mustBeRow}
     vars_i (1, :) double {mustBeRow}
+    opts.MaxIter = 1000
 end
 
 [~, c] = size(vars);
@@ -10,14 +11,18 @@ A = size(c);
 b = zeros(c, 1);
 iter = 1;
 
-while iter < 100
+const_vars_i = vars_i;
+
+while iter < opts.MaxIter
     for i=1:c
         for j=1:c
-            A(i, j) = subs(diff(funcs(i), vars(j)), vars_i(j));
+            A(i, j) = subs(diff(funcs(i), vars(j)), const_vars_i(j));
         end
         b(i) = -1 * subs(funcs(i), vars, vars_i);
     end
+
     soln = A \ b;
+
     if sum(abs(soln)) < 1e-4
         break;
     end
