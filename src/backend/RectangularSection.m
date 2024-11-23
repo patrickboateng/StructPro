643bd2c43ef381a276f3df5a783c94db838e564a
Patrick Boateng <boatengpato.pb@gmail.com>
2024-11-23 00:20:50 -0600

    removed object constructor

--- src/backend/RectangularSection.m
+++ src/backend/RectangularSection.m
@@ -5,16 +5,16 @@ classdef RectangularSection < Section
         A 
     end
 
-    methods
-        function obj = RectangularSection(b, d, E)  
-            arguments
-                b 
-                d 
-                E = 200e9;
-            end
-            obj.E = E;
-            obj.I = (b * d^3) / 12;
-            obj.A = b * d;
-        end
-    end
+    % methods
+    %     function obj = RectangularSection(b, d, E)  
+    %         arguments
+    %             b 
+    %             d 
+    %             E = 200e9;
+    %         end
+    %         obj.E = E;
+    %         obj.I = (b * d^3) / 12;
+    %         obj.A = b * d;
+    %     end
+    % end
 end
