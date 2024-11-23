299892558f33fb9905316bd41981b3387256789f
Patrick Boateng <boatengpato.pb@gmail.com>
2024-11-23 00:19:45 -0600

    added fixed end force and moment

--- src/backend/Node.m
+++ src/backend/Node.m
@@ -1,14 +1,17 @@
-classdef Node
+classdef Node < handle
     properties 
         id {mustBeInteger}
         position Point2D
         support  Support
         point_load PointLoad
         point_moment PointMoment
+        fixed_end_force PointLoad
+        fixed_end_moment PointMoment
     end
 
     methods
-        function obj = Node(id, x, y, point_load, point_moment, rx, ry, rm)
+        function obj = Node(id, x, y, point_load, ...
+                            point_moment, rx, ry, rm)
             arguments
                 id {mustBeInteger}
                 x {mustBeNumeric}
@@ -17,15 +20,16 @@ classdef Node
                 point_moment double;
                 rx {mustBeNumericOrLogical} = 0
                 ry {mustBeNumericOrLogical} = 0
-                rm {mustBeNumericOrLogical} = 0
-                
+                rm {mustBeNumericOrLogical} = 0                
             end
-
             obj.id = id;
             obj.position = Point2D(x, y);
             obj.support = Support(rx, ry, rm);
             obj.point_load = PointLoad(point_load, obj);
             obj.point_moment = PointMoment(point_moment, obj);
+
+            obj.fixed_end_force = PointLoad(0, obj);
+            obj.fixed_end_moment = PointMoment(0, obj);
         end
 
     end
