f0bf6e944aa0c9aa5be6f849ada0cf1ea7f269ab
Patrick Boateng <boatengpato.pb@gmail.com>
2024-12-16 23:55:13 -0600

    added a constructor and validators to properties

--- src/backend/RectangularSection.m
+++ src/backend/RectangularSection.m
@@ -1,20 +1,20 @@
-classdef RectangularSection < Section
+classdef RectangularSection
     properties
-        I 
-        E
-        A 
+        I {mustBePositive, mustBeNumeric}
+        E {mustBePositive, mustBeNumeric}
+        A {mustBePositive, mustBeNumeric}
     end
 
-    % methods
-    %     function obj = RectangularSection(b, d, E)  
-    %         arguments
-    %             b 
-    %             d 
-    %             E = 200e9;
-    %         end
-    %         obj.E = E;
-    %         obj.I = (b * d^3) / 12;
-    %         obj.A = b * d;
-    %     end
-    % end
+    methods
+        function obj = RectangularSection(b, d, E)  
+            arguments
+                b {mustBePositive, mustBeNumeric}
+                d {mustBePositive, mustBeNumeric}
+                E {mustBePositive, mustBeNumeric}
+            end
+            obj.I = (b * d^3) / 12;
+            obj.A = b * d;
+            obj.E = E;
+        end
+    end
 end
