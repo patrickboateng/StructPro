8653a4d5b8fa7348f00ece01a97a76211ca1d1e8
Patrick Boateng <boatengpato.pb@gmail.com>
2024-12-18 16:43:14 -0600

    made properties immutable

--- src/backend/Section.m
+++ src/backend/Section.m
@@ -1,8 +1,15 @@
-classdef (Abstract) Section
-    properties (Abstract)
+classdef Section
+    properties (SetAccess=immutable)
         I {mustBeNumeric}
-        E {mustBeNumeric}
         A {mustBeNumeric}
+        E {mustBeNumeric}
     end
 
+    methods
+        function obj = Section(I, A, E)
+            obj.I = I;
+            obj.A = A;
+            obj.E = E;
+        end
+    end
 end
