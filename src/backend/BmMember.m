d61366136d127bab69cbad221cd19df8c9a30a30
Patrick Boateng <boatengpato.pb@gmail.com>
2024-12-18 15:46:59 -0600

    inherited from member class

--- src/backend/BmMember.m
+++ src/backend/BmMember.m
@@ -1,25 +1,5 @@
-classdef BmMember
-    properties
-        id {mustBePositive, mustBeInteger}
-        start_node BmNode
-        end_node BmNode
-        section RectangularSection
-    end
-
+classdef BmMember < Member
     methods
-        function obj = BmMember(id, start_node, end_node, section)
-            arguments
-                id
-                start_node
-                end_node
-                section
-            end
-            obj.id = id;
-            obj.start_node = start_node;
-            obj.end_node = end_node;
-            obj.section = section;
-        end
-
         function len = len(obj)
             len = obj.end_node.position.x - obj.start_node.position.x;
         end
@@ -31,10 +11,10 @@ classdef BmMember
 
             k = (E * I) / L^3;
 
-            k_local = [12   6*L    -12    6*L; ...
-                       6*L  4*L^2  -6*L   2*L^2; ...
-                       -12  -6*L     12   -6*L; ...
-                       6*L  2*L^2  -6*L   4*L^2];
+            k_local = [12    6*L    -12    6*L; ...
+                       6*L   4*L^2  -6*L   2*L^2; ...
+                       -12   -6*L     12   -6*L; ...
+                       6*L   2*L^2  -6*L   4*L^2];
 
             k_local = k_local .* k;
         end
