a2ad311747b9fea14ec52db3916ebecacc3840b9
Patrick Boateng <boatengpato.pb@gmail.com>
2024-12-18 16:04:42 -0600

    added load direction

--- src/backend/PointLoad.m
+++ src/backend/PointLoad.m
@@ -2,16 +2,19 @@ classdef PointLoad < handle
     properties
         magnitude {mustBeNumeric}
         position Point2D
+        direction LoadDirection
     end
 
     methods
-        function obj = PointLoad(magnitude, node)
+        function obj = PointLoad(magnitude, node, direction)
             arguments
                 magnitude (1, 1) {mustBeNumeric}
-                node (1, 1) 
+                node (1, 1) Node
+                direction (1, 1) LoadDirection = LoadDirection.VERTICAL
             end
             obj.magnitude = magnitude;
-            obj.position = node.position;      
+            obj.position = node.position; 
+            obj.direction = direction;
         end
     end
 end
