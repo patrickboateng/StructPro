1dd9964a45ef964f3164e79975b3870383fb923e
Patrick Boateng <boatengpato.pb@gmail.com>
2024-12-22 19:56:24 -0600

    test file

--- src/backend/main.m
+++ src/backend/main.m
@@ -1,96 +1,93 @@
 clc;clearvars;
 
 section = RectangularSection();
-section.I = 108e-6;
-section.E = 200e9;
-
-node_1 = Node(1, 0, 0, 0, 0, 1, 1, 1);
-node_2 = Node(2, 2, 0, -150, 0, 0, 0, 0);
-node_3 = Node(3, 4, 0, 0, 0, 0, 1, 0);
-node_4 = Node(4, 7, 0, 0, 0, 0, 1, 0);
-node_5 = Node(5, 11, 0, 0, 0, 1, 1, 1);
-
-member_1 = Member(1, node_1, node_2, section, 0);
-member_2 = Member(2, node_2, node_3, section, 0);
-member_3 = Member(3, node_3, node_4, section, 0);
-member_4 = Member(4, node_4, node_5, section, -37.5);
-
-nodes = [node_1, node_2, node_3, node_4, node_5];
-members = [member_1, member_2, member_3, member_4];
+section.setI(1.000E-04);
+section.setA(1.000E-02);
 
 beam = Beam2D(11);
 
-beam.add_nodes(nodes);
-beam.add_members(members);
+node_1 = BmNode(1, 0, 0, 0, 0, 1, 1, 1);
+node_2 = BmNode(2, 2, 0, -150, 0, 0, 0, 0);
+node_3 = BmNode(3, 4, 0, 0, 0, 0, 1, 0);
+node_4 = BmNode(4, 7, 0, 0, 0, 0, 1, 0);
+node_5 = BmNode(5, 11, 0, 0, 0, 1, 1, 1);
 
-% beam = Beam(32);
-%
-% node_1 = Node(1, 0, 0, 0, 0, 1, 1, 1);
-% node_2 = Node(2, 24, 0, 0, 0, 0, 1, 0);
-% node_3 = Node(3, 28, 0, -12, 0, 0, 0, 0);
-% node_4 = Node(4, 32, 0, 0, 0, 0, 1, 0);
-%
-% beam.add_nodes([node_1, node_2, node_3, node_4]);
-%
-% member_1 = Member(1, node_1, node_2, section, -2);
-% member_2 = Member(2, node_2, node_3, section, 0);
-% member_3 = Member(3, node_3, node_4, section, 0);
-%
-% beam.add_members([member_1, member_2, member_3]);
+elem_1 = BmMember(1, node_1, node_2, section);
+elem_2 = BmMember(2, node_2, node_3, section);
+elem_3 = BmMember(3, node_3, node_4, section);
+elem_4 = BmMember(4, node_4, node_5, section);
 
-beam = Beam2D(4);
+udl = UniformDistributedLoad(-37.5, -37.5, node_4, node_5);
 
-node_1 = Node(1, 0, 0, 0, 0, 1, 1, 1);
-node_2 = Node(2, 2, 0, -8, 0, 0, 0, 0);
-node_3 = Node(3, 4, 0, -3, 0, 0, 0, 0);
+beam.add_nodes([node_1, node_2, node_3, node_4, node_5]);
+beam.add_members([elem_1, elem_2, elem_3, elem_4]);
+beam.add_distributed_load(udl);
 
-beam.add_nodes([node_1, node_2, node_3]);
-
-member_1 = Member(1, node_1, node_2, section, 0);
-member_2 = Member(2, node_2, node_3, section, 0);
-
-beam.add_members([member_1, member_2]);
-
-beam = StaticSolver2D.solve(beam);
-
-for i=1:length(beam.nodes)
-    disp([i beam.nodes(i).reaction_moment.magnitude])
-end
-
-function shear_force = calc_shear_force(member, node, start_force)
-
-point_load = node.point_load.magnitude;
-reaction_force = node.reaction_force.magnitude;
-
-distributed_load = 0;
-
-if node.id ~= 1
-    m_len = member.len();
-    distributed_load = member.distributed_load.magnitude;
-    distributed_load = distributed_load * m_len;
-end
+% beam = Beam2D(10);
+% node_1 = BmNode(1, 0,  0,  0, 0, 1, 1, 1);
+% node_2 = BmNode(2, 10, 0,  0, 0, 0, 0, 0);
+% member_1 = BmMember(1, node_1, node_2, section);
+% beam.add_nodes([node_1, node_2]);
+% beam.add_members(member_1);
+% 
+% udl = UniformDistributedLoad(-10, -10, node_1, node_2);
+% beam.add_distributed_load(udl);
 
 
-% ext_force = distributed_load;
+[x, bm] = beam.calc_bending_moment();
+[~, sf] = beam.calc_shear_force();
+[~, sl] = beam.calc_slope();
+[~, df] = beam.calc_deflection();
 
-start_force = start_force + distributed_load;
-end_force = start_force + point_load + reaction_force;
-shear_force = [start_force, end_force];
-shear_force
-end
+subplot(4, 1, 1)
+area(x, sf)
+subplot(4, 1, 2)
+area(x, bm)
+subplot(4, 1, 3)
+area(x, sl)
+subplot(4, 1, 4)
+area(x, df)
 
-prev_force = 0;
-% shear_forces = [];
-for i=1:length(beam.members)
-    member = beam.members(i);
-
-    if i == 1
-        node = member.start_node;
-        curr_force = calc_shear_force(member, node, prev_force);
-    end
-
-    prev_force = curr_force(2);
-
-    node = member.end_node;
-    curr_force = calc_shear_force(member, node, prev_force);
-end
+% % Numerical integration to find slope
+% theta = cumtrapz(x, bm ./ (section.getE() * section.getI())); % Cumulative integration for slope
+% 
+% % Numerical integration to find deflection
+% v = cumtrapz(x, theta); % Cumulative integration for deflection
+% 
+% % Adjust for boundary conditions
+% % Assume deflection and slope are zero at x = 0
+% v = v - v(1); % Adjust deflection to ensure v(0) = 0
+% theta = theta - theta(1); % Adjust slope to ensure theta(0) = 0
+% 
+% % Plot results
+% figure;
+% 
+% % Plot bending moment
+% subplot(3, 1, 1);
+% plot(x, bm, 'r-o', 'LineWidth', 1.5);
+% xlabel('x (m)');
+% ylabel('M(x) (Nm)');
+% title('Bending Moment');
+% grid on;
+% 
+% % Plot slope
+% subplot(3, 1, 2);
+% plot(x, theta, 'b-o', 'LineWidth', 1.5);
+% xlabel('x (m)');
+% ylabel('\theta(x) (rad)');
+% title('Slope');
+% grid on;
+% 
+% % Plot deflection
+% subplot(3, 1, 3);
+% plot(x, v, 'g-o', 'LineWidth', 1.5);
+% xlabel('x (m)');
+% ylabel('v(x) (m)');
+% title('Deflection');
+% grid on;
+% 
+% % Display results
+% disp('Slope values (rad):');
+% % disp(theta);
+% disp('Deflection values (m):');
+% % disp(v);
